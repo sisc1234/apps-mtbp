9faf75f8a5e02d70ca1df3c4e04f80937f41f2b8
Sunil Yadav <sunil.yadav2@gmail.com>
2022-05-03 10:22:14 +0530

    Add Swagger + Datasource configs
    Add Swagger + Datasource configs

--- src/main/java/com/apps/mtbp/MtbpApplication.java
+++ src/main/java/com/apps/mtbp/MtbpApplication.java
@@ -9,5 +9,5 @@ public class MtbpApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MtbpApplication.class, args);
 	}
-
+	
 }
---
+++ src/main/java/com/apps/mtbp/common/BeansConfig.java
@@ -0,0 +1,32 @@
+package com.apps.mtbp.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class BeansConfig {
+
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption,
+			@Value("${application-version}") String appVersion) {
+
+		List<Server> servers = new ArrayList<Server>();
+
+		servers.add(new Server().description("Mtbp Local Server").url("http://localhost:8080/"));
+
+		return new OpenAPI().servers(servers)
+				.info(new Info().title("Mtbp API Specification").version(appVersion)
+						.description(appDesciption).termsOfService("http://swagger.io/terms/")
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+	}
+
+}
