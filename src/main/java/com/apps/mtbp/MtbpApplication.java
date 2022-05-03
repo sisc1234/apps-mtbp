ce4b0f59677961438cca444055b496ba0f9fbef3
Sunil Yadav <sunil.yadav2@gmail.com>
2022-05-03 09:31:37 +0530

    Skelton code drop
    Skelton code drop

---
+++ src/main/java/com/apps/mtbp/MtbpApplication.java
@@ -0,0 +1,13 @@
+package com.apps.mtbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtbpApplication.class, args);
+	}
+
+}
